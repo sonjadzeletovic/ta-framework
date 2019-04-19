73085b90c3813ac8a73f759d099891940abe5de6
sonjadzeletovic <sonjadz89@gmail.com>
2019-04-19 13:53:54 +0200

    5 cas

--- src/main/java/com/test/workshop/testTA/webDriver/WebDriverFactory.java
+++ src/main/java/com/test/workshop/testTA/webDriver/WebDriverFactory.java
@@ -1,15 +1,46 @@
 package com.test.workshop.testTA.webDriver;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 
 public class WebDriverFactory {
 
     public static WebDriver getDriver() {
-        //Incijalizacija web drivera
+  /*      //Incijalizacija web drivera
         System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver");
         WebDriver driver = new ChromeDriver();
-        driver.manage().window().maximize();
+        driver.manage().window().maximize();*/
+
+  //podrska za 4 browser-a
+        WebDriver driver = null;
+        String browser = System.getProperty("browser");
+        System.out.println("xxx"+browser);
+        String chromeBrowser = "chrome";
+        String firefoxBrowser = "firefox";
+        String edgeBrowser = "edge";
+        String ieBrowser = "explorer";
+
+      /*  WebDriverManager.chromedriver().setup(); //storuje u maven putanji
+        driver = new ChromeDriver();*/
+        if (browser.equalsIgnoreCase(chromeBrowser)) {
+            WebDriverManager.chromedriver().setup(); //storuje u maven putanji
+            driver = new ChromeDriver();
+        } else if (browser.equalsIgnoreCase(firefoxBrowser)) {
+            WebDriverManager.firefoxdriver().setup();
+            driver = new FirefoxDriver();
+        } else if (browser.equalsIgnoreCase(edgeBrowser)) {
+            WebDriverManager.edgedriver().setup();
+            driver = new EdgeDriver();
+        } else if (browser.equalsIgnoreCase(ieBrowser)) {
+            WebDriverManager.iedriver().setup();
+            driver = new InternetExplorerDriver();
+        } else {
+            System.out.println("Uneli smo nepodrzani browser");
+        }
 
         return driver;
     }
